48d27c4670365c849f3d64e33463358860773121
Naresh <phoenixarjun007@gmail.com>
2025-06-20 15:13:26 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'NARESH B A',
+    author_email= 'phoenixarjun007@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
