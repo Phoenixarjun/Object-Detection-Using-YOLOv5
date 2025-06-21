46400f409542301db3ed0a459322772c8a1aac23
Naresh <phoenixarjun007@gmail.com>
2025-06-21 15:08:04 +0530

    data ingestion done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Object-Detection-Using-YOLOv5
+
+
+## Workflows
+
+1. constants
+2. entity
+3. components
+4. pipelines
+5. app.py
---
+++ app.py
@@ -0,0 +1,7 @@
+from wasteDetection.pipeline.training_pipeline import TrainPipeline
+
+
+
+obj = TrainPipeline()
+
+obj.run_pipeline()
